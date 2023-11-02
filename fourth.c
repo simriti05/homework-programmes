fdc77761ac3896a577b90054688758ee3ccfd8b1
simriti05 <146444829+simriti05@users.noreply.github.com>
2023-11-02 22:39:38 +0530

    Add files via upload

---
+++ fourth.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main () {
+    int i,j,k,h;
+    for(i=1;i<=5;i++)
+    j=1+2+3+4+5;
+    {
+        for(i=1;i<=5;i++)
+        {
+            printf("square of the number is: %d\n",i*i);
+        }
+        printf("sum of first five numbers is: %d\n",j);
+        for(i=1;i<=5;i++)
+        h=1+4+9+16+25;
+        {
+            printf("sum of squares is: %d",h);
+        }
+    }
+    return 0;
+}
